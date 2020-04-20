b1b736d1ff1ada24394cf9803c43e99ab3fa40dd
Jonas Plum <jonas.plum@siemens.com>
2020-04-20 23:52:22 +0200

    Add tests

---
+++ collection/process_test.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2020 Siemens AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Author(s): Jonas Plum
+ */
+
+package collection
+
+import (
+	"github.com/forensicanalysis/forensicstore/goforensicstore"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestLiveCollector_createProcess(t *testing.T) {
+	testDir := setup(t)
+	defer teardown(t, testDir)
+
+	store, err := goforensicstore.NewJSONLite(filepath.Join(testDir, "store"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type fields struct {
+		Store   *goforensicstore.ForensicStore
+		TempDir string
+	}
+	type args struct {
+		definitionName string
+		cmd            string
+		args           []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *goforensicstore.Process
+	}{
+		{"hostname", fields{store, testDir}, args{"test", "hostname", nil}, &goforensicstore.Process{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &LiveCollector{
+				Store:   tt.fields.Store,
+				TempDir: tt.fields.TempDir,
+			}
+			if got := c.createProcess(tt.args.definitionName, tt.args.cmd, tt.args.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createProcess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ collection/wmi_windows_test.go
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2020 Siemens AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Author(s): Jonas Plum
+ */
+
+package collection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWMIQuery(t *testing.T) {
+	type args struct {
+		q string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"OS", args{"SELECT * from Win32_OperatingSystem"}, "C:\\WINDOWS", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := WMIQuery(tt.args.q)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WMIQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got[0]["WindowsDirectory"], tt.want) {
+				t.Errorf("WMIQuery() got = %v, want %v", got[0]["WindowsDirectory"], tt.want)
+			}
+		})
+	}
+}
