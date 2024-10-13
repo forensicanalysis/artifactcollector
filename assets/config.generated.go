24ac36e86233781155641b27b864876449fda81d
Jonas Plum <git@cugu.eu>
2024-10-14 00:48:54 +0200

    fix: import

--- assets/config.generated.go
+++ assets/config.generated.go
@@ -1,7 +1,5 @@
 package assets
 
-import (
-	"github.com/forensicanalysis/artifactcollector/collector"
-)
+import "github.com/forensicanalysis/artifactcollector/collector"
 
 var Config = &collector.Configuration{Artifacts: []string{"DefaultCollection1"}, User: false, Case: "", OutputDir: ""}
